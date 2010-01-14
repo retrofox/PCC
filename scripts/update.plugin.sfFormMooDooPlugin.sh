060b062c8b759a527fc9dbdfdf90f1e102f2622d
RetroFox <damian.suarez@xifox.net>
2010-01-13 21:16:29 -0300

    Agregamos scripts bash para actualizar los plugins. Ya no usamos enlaces simbolicos.

---
+++ scripts/update.plugin.sfFormMooDooPlugin.sh
@@ -0,0 +1,2 @@
+cp /var/www/sfFormMooDooPlugin/trunk/plugin/ ../project/plugins/sfFormMooDooPlugin/ -R
+chown www-data:www-data ../project/plugins/sfFormMooDooPlugin/ -R
---
+++ scripts/update.plugin.sfPropelMooDooPlugin.sh
@@ -0,0 +1,3 @@
+cp /var/www/sfPropelMooDooPlugin/trunk/plugin/ ../project/plugins/sfPropelMooDooPlugin/ -R
+chown www-data:www-data ../project/plugins/sfPropelMooDooPlugin/ -R
+
